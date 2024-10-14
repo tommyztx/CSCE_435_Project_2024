540a3a3111e9191cbb58c853401d190ceb572ad7
Albert Yin <albertapplea@gmail.com>
2024-10-14 18:45:50 -0500

    initial implementation of bitonic sort

--- MPI_Builds/src/bitonicsort.cpp
+++ MPI_Builds/src/bitonicsort.cpp
@@ -1,5 +1,29 @@
 #include "bitonicsort.h"
 
-void bitonic_sort(unsigned int* arr, unsigned int n) {
-    printf("BITONIC SORT\n");
+void bitonic_merge(unsigned int *arr, unsigned int low, unsigned int cnt, bool dir)
+{
+    if (cnt > 1)
+    {
+        unsigned int k = cnt / 2;
+        for (unsigned int i = low; i < low + k; i++)
+        {
+            if (dir == (arr[i] > arr[i + k]))
+            {
+                std::swap(arr[i], arr[i + k]);
+            }
+        }
+        bitonic_merge(arr, low, k, dir);
+        bitonic_merge(arr, low + k, k, dir);
+    }
+}
+
+void bitonic_sort(unsigned int *arr, unsigned int low, unsigned int cnt, bool dir)
+{
+    if (cnt > 1)
+    {
+        unsigned int k = cnt / 2;
+        bitonic_sort(arr, low, k, true);
+        bitonic_sort(arr, low + k, k, false);
+        bitonic_merge(arr, low, cnt, dir);
+    }
 }
